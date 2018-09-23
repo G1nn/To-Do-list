6952b19962d82b9c18cd995ad1caf66710e2102c
G1nn <luxxx917@umn.edu>
2018-09-23 10:33:16 -0500

    Add files via upload

---
+++ js/todolist.js
@@ -0,0 +1,26 @@
+//complete an item
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//delete an item
+$("ul").on("click", "span", function(){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	})
+	event.stopPropagation();
+})
+
+//add an item
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13){
+		var itemText = $(this).val();
+		$(this).val(""); 
+		$("ul").append("<li><i class='fas fa-trash'> " + itemText + "</li>");
+	}
+})
+
+//plus sign toggle
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+})
